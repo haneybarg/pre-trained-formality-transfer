7d50027fad96e61dcf19698c3d3ff88e419db954
fernandaguimaraes28 <fernandaguimaraes28@gmail.com>
2023-11-03 17:23:11 +0800

    Add missing directory and fix typo

--- pg.sh
+++ pg.sh
@@ -17,7 +17,7 @@ python classifier/test.py -dataset $1 -order $2.0
 
 echo "----------------BLEU----------------"
 python utils/tokenizer.py data/outputs/bart_$1_$2.0.0 data/$1/outputs/bart_$1_$2.0.0 False
-python utils/tokenizer.py data/outputs/bart_$1_$2.0.1 data/$1/outputs/bart_$1_$2.0.1 Flase
+python utils/tokenizer.py data/outputs/bart_$1_$2.0.1 data/$1/outputs/bart_$1_$2.0.1 False
 perl utils/multi-bleu.perl data/$1/original_ref/$5.ref < data/$1/outputs/bart_$1_$2.0.0
 perl utils/multi-bleu.perl data/$1/original_ref/$6.ref < data/$1/outputs/bart_$1_$2.0.1
 
