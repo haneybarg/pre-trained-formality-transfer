3488a90724aa8f4cc6a345ce6c166e2f85e4eb37
fernandaguimaraes28 <fernandaguimaraes28@gmail.com>
2023-11-07 14:53:54 -0300

    Fix script, adding parameters

--- pg.sh
+++ pg.sh
@@ -12,15 +12,15 @@ echo "----------------Style----------------"
 python classifier/test.py -dataset $1 -order $2.0
 
 echo "----------------BLEU----------------"
-python utils/tokenizer.py data/em/test/formal.ref0 data/em/original_ref/formal.ref0 False
-python utils/tokenizer.py data/em/test/formal.ref1 data/em/original_ref/formal.ref1 False
-python utils/tokenizer.py data/em/test/formal.ref2 data/em/original_ref/formal.ref2 False
-python utils/tokenizer.py data/em/test/formal.ref3 data/em/original_ref/formal.ref3 False
+python utils/tokenizer.py data/$1/test/formal.ref0 data/$1/original_ref/formal.ref0 False
+python utils/tokenizer.py data/$1/test/formal.ref1 data/$1/original_ref/formal.ref1 False
+python utils/tokenizer.py data/$1/test/formal.ref2 data/$1/original_ref/formal.ref2 False
+python utils/tokenizer.py data/$1/test/formal.ref3 data/$1/original_ref/formal.ref3 False
 
 python utils/tokenizer.py outputs/bart_$1.0.txt outputs/bart_$1_$2.0.0 False
 perl utils/multi-bleu.perl data/$1/original_ref/$5.ref < outputs/bart_$1_$2.0.0
 
 # echo "----------------BLEURT----------------"
-# python utils/cal_bleurt.py data/outputs/bart_$1_$2.0.0 data/outputs/bart_$1_$2.0.1 \
-#                          data/$1/test/$5.ref data/$1/test/$6.ref
+python utils/cal_bleurt.py outputs/bart_$1_$2.0.0 outputs/bart_$1_$2.0.1 \
+                         data/$1/test/$5.ref data/$1/test/$6.ref
 
